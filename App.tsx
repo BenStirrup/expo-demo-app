68e58492d948cdc0ee868010c79fd69301a9a1c6
Benjamin Robert Stirrup <benjamin.stirrup@gmail.com>
2020-09-17 16:27:09 +0200

    Integrate a map with mapbox (android only)

--- App.tsx
+++ App.tsx
@@ -1,21 +1,37 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import env from "react-native-dotenv";
+import MapboxGL from "@react-native-mapbox-gl/maps";
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+MapboxGL.setAccessToken(env.MAPBOX_TOKEN);
+MapboxGL.setTelemetryEnabled(false);
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+    <View style={styles.root}>
+      <Text>Demo App</Text>
+      <View style={styles.mapContainer}>
+        <Text>Mapbox</Text>
+        <MapboxGL.MapView style={styles.map} />
+      </View>
       <StatusBar style="auto" />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  root: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  map: {
+    flex: 1,
+  },
+  mapContainer: {
+    height: 300,
+    width: 300,
   },
 });
